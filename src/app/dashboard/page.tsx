6cb64452ca0f417a68c1fc117cd656dffbe26b11
Ilyat9 <afrom205@gmail.com>
2026-03-04 19:33:21 +0300

    feat: set dashboard to force-dynamic rendering

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { useEffect, useState } from 'react'
 import { useAuth } from '@/features/auth/ui/useAuth'
 import { Button } from '@/shared/ui/Button'
